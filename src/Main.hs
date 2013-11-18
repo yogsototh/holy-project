d8cc2de5a16d6073762c524b9995183d1f3c5cc1
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2013-11-18 09:45:28 +0100

    added questions

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import System.Console.ANSI
+import System.IO            (hFlush, stdout)
 
 output :: ConsoleIntensity -> ColorIntensity -> Color -> String -> IO ()
 output bold intensity color str = do
@@ -26,6 +27,32 @@ intro = do
     bk "ere the other side he see."
     you "Ask me the questions, bridgekeeper, I am not afraid.\n"
 
+end :: IO ()
+end = do
+    putStrLn "\n\n"
+    bk "What... is the air-speed velocity of an unladen swallow?"
+    you "What do you mean? An African or European swallow?"
+    bk "Huh? I... I don't know that."
+    putStrLn "[the bridgekeeper is thrown over]"
+    bk "Auuuuuuuuuuuugh"
+    putStrLn "Sir Bedevere: How do you know so much about swallows?"
+    you "Well, you have to know these things when you're a king, you know."
+
+ask :: String -> IO String
+ask info = do
+    bk $ "What is your " ++ info ++ "?"
+    putStr "> "
+    hFlush stdout
+    answer <- getLine
+    putStrLn ""
+    return answer
+
 main :: IO ()
 main = do
     intro
+    ask "project name"
+    ask "name"
+    ask "email"
+    ask "github account"
+    ask "project in less than a dozen word?"
+    end
